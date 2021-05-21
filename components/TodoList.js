0a93dbfe80ebd7b5b87822ae37b302852268bf21
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-05-21 17:17:41 +0200

    Add GetTodos and capitalize addTodo mutationt

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,16 +1,28 @@
+import { gql, useQuery } from '@apollo/client'
+
 import SingleTodo from '../components/SingleTodo'
 
+export const GET_TODOS = gql`
+  query GetTodos {
+    todos(order_by: { created_at: desc }) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
 const TodoList = () => {
-  const todos = [
-    { title: "hello", id: "1", completed: false },
-    { title: "hello 2", id: "2", completed: false },
-    { title: "hello 3", id: "3", completed: true },
-  ];
+  const { loading, error, data } = useQuery(GET_TODOS);
+
+  if (loading) return "Loading...";
+
+  if (error) return <>{console.log(error)}</>;
 
   return (
     <div className='overflow-hidden bg-white rounded-md shadow'>
       <ul className='divide-y divide-gray-200'>
-        {todos.map((todo) => (
+        {data?.todos.map((todo) => (
           <SingleTodo key={todo.id} todo={todo} />
         ))}
       </ul>
